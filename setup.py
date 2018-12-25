54b59e11786a0b952c5594af88d132de27cb0941
stepheku <isomere@gmail.com>
2018-12-24 22:58:52 -0500

    added custom commands to setup.py added custom commands to install unrtf via apt-get to setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,18 @@
 import os
-
 from setuptools import setup, find_packages
+
 
 here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.md')) as f:
     README = f.read()
 with open(os.path.join(here, 'CHANGES.txt')) as f:
     CHANGES = f.read()
+
+# TODO: Add the custom commands to install the unrtf system dependency
+CUSTOM_COMMANDS = [
+    ['apt-get', 'update'],
+    ['apt-get', '--assume-yes', 'install', 'unrtf'],
+]
 
 requires = [
     'pyramid',
